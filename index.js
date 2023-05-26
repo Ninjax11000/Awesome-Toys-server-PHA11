53dd97a4028d026f38115fe361b275e918b27a50
Ninjax11000 <jamiljoy007@gmail.com>
2023-05-26 11:36:14 +0600

    fetced data from mongodb

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toyMarket').collection('toys');
+
+    app.get('/toys',async(req,res)=>{
+        const cursor =toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
